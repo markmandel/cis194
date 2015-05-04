fb5600b5cc1ade287f26f846b875a58b1aeea39e
Mark Mandel <mark.mandel@gmail.com>
2015-05-04 14:52:11 +1000

    More fun with functions and guards.

--- week1/lecture/functions.hs
+++ week1/lecture/functions.hs
@@ -3,7 +3,22 @@ sumtorial 0 = 0
 sumtorial n = n + sumtorial (n -1)
 
 
+-- extra guards
 hailstone :: Integer -> Integer
 hailstone n 
 	| mod n 2 == 0 = div n 2
-	| otherwise 	 = (3 * n) + 1  
+	| otherwise 	 = (3 * n) + 1
+	
+-- you can also mix and match guard statements
+foo :: Integer -> Integer
+foo 0 = 16
+foo 1 
+  | "Haskell" > "C++" = 3
+  | otherwise         = 4
+foo n
+  | n < 0            = 0
+  | n `mod` 17 == 2  = -43
+  | otherwise        = n + 3
+  
+isEven :: Integer -> Bool
+isEven n = mod n 2 == 0
