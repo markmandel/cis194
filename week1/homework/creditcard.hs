ffaf626383dfdc83a30a65c3b1eb0f3c6400e377
Mark Mandel <mark.mandel@gmail.com>
2015-05-04 21:47:16 +1000

    Worked out digiting, but should clean it up to match excercise.

--- week1/homework/creditcard.hs
+++ week1/homework/creditcard.hs
@@ -5,3 +5,19 @@ doubleSecond(x:(y:rest)) = x : (2 * y) : doubleSecond rest
 
 doubleSecondLast :: [Integer] -> [Integer]
 doubleSecondLast n = reverse (doubleSecond (reverse n))
+
+listDigits :: Integer -> [Integer]
+listDigits 0 = []
+listDigits n = rhsDigit n : listDigits (remainingDigits n (rhsDigit n))
+
+
+-- remaining digits. Original, RHSDigit, returns the result
+remainingDigits :: Integer -> Integer -> Integer
+remainingDigits original rhs = (original - rhs) `div` 10
+
+-- result is digit, remaining
+rhsDigit :: Integer -> Integer
+rhsDigit n 
+	| n < 10 = n
+	| otherwise = n - ((n `div` 10) * 10)
+	
