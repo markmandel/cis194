981006089790edfc2318cb700f93bfddf274509e
Mark Mandel <mark.mandel@gmail.com>
2015-05-04 18:03:21 +1000

    Beginning of list comprehension.

---
+++ week1/lecture/lists.hs
@@ -0,0 +1,21 @@
+range :: [Integer]
+range = [1..100]
+
+-- cons operator
+onetwothree :: [Integer]
+onetwothree = 1 : 2 : 3 : []
+
+hailstone :: Integer -> Integer
+hailstone n
+  | n `mod` 2 == 0 = n `div` 2
+  | otherwise      = 3*n + 1
+  
+hailstoneSeq :: Integer -> [Integer]
+hailstoneSeq 1 = [1]
+hailstoneSeq n = n : hailstoneSeq (hailstone n)
+
+-- pattern matching on lists
+intListLen :: [Integer] -> Integer
+intListLen [] = 0
+-- similar pattern matchin gto tuples. Interesting!
+intListLen (x:xs) = 1 + intListLen xs
