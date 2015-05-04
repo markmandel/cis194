895a4b4c0af9813253971c69677fc279d531eeee
Mark Mandel <mark.mandel@gmail.com>
2015-05-04 21:13:46 +1000

    Do it from the RHS

--- week1/homework/creditcard.hs
+++ week1/homework/creditcard.hs
@@ -1,5 +1,7 @@
-
 doubleSecond :: [Integer] -> [Integer]
 doubleSecond [] = []
 doubleSecond (x:[]) = [x]
 doubleSecond(x:(y:rest)) = x : (2 * y) : doubleSecond rest
+
+doubleSecondLast :: [Integer] -> [Integer]
+doubleSecondLast n = reverse (doubleSecond (reverse n))
