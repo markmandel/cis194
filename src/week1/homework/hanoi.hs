193dec4a34b56d6f22c600267ffe2450a60fa799
Mark Mandel <mark.mandel@gmail.com>
2015-05-07 21:57:11 -0700

    Towers of Hanoi solution.

--- src/week1/homework/hanoi.hs
+++ src/week1/homework/hanoi.hs
@@ -1,6 +1,20 @@
 type Peg = String
 type Move = (Peg, Peg)
 
+{-
+ public void solve(int n, String start, String auxiliary, String end) {
+       if (n == 1) {
+           System.out.println(start + " -> " + end);
+       } else {
+           solve(n - 1, start, end, auxiliary);
+           System.out.println(start + " -> " + end);
+           solve(n - 1, auxiliary, start, end);
+       }
+   }
+-}
+
 -- disks, 3 pegs. Returns a set of moves
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
-hanoi n p1 p2 p3 = []
+hanoi n start aux end
+    | n <= 1 = [(start, end)]
+    | otherwise = concat [hanoi (n - 1) start end aux, [(start, end)], hanoi (n - 1) aux start end]
