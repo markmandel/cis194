5d7a083a40456c3d95339689757c51f567e55d44
Mark Mandel <mark.mandel@gmail.com>
2015-05-16 22:36:44 -0700

    Doubles type

---
+++ src/week2/lecture/doubles.hs
@@ -0,0 +1,11 @@
+data FailableDouble = Failure
+                    | OK Double
+  deriving Show
+
+safeDiv :: Double -> Double -> FailableDouble
+safeDiv _ 0 = Failure
+safeDiv x y = OK (x / y)
+
+failureToZero :: FailableDouble -> Double
+failureToZero Failure = 0
+failureToZero (OK d)  = d
